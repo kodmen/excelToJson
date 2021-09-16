c3e6a8a0c4f7abe290acdb5ff94bc1fd303b9f54
bedirhan ergün <bedirhan1029@gmail.com>
2021-09-16 17:06:16 +0300

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+V
+&src/main/java/com/morphosium/main.java,0/d/0d55215cb287b7d1cf0bc5a5c09580d614cb7c72
---
+++ src/main/java/com/morphosium/main.java
@@ -0,0 +1,99 @@
+package com.morphosium;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class main {
+
+    public static HashMap rowLengthCalculator(Row row) {
+        int temp = 0;
+        HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+        for (int i = 0; i < row.getLastCellNum(); i++) {
+            if (!row.getCell(i).toString().equals("")) {
+                temp++;
+                hmap.put(i, row.getCell(i).toString());
+            }
+        }
+        return hmap;
+    }
+
+    public static HashMap searchLenguage(Workbook workbook) {
+        HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+        int temp = 0;
+
+        for (int i = 1; i < workbook.getNumberOfSheets(); i++) {
+
+            Sheet sheet = workbook.getSheetAt(i);
+
+            for (Row row : sheet) {
+
+                for (int j = 0; j < row.getLastCellNum(); j++) {
+
+                    if (row.getCell(0).getStringCellValue().equals("Key")) {
+                        hmap = rowLengthCalculator(row);
+                        i++;
+                    }
+                }
+            }
+        }
+        return hmap;
+    }
+
+    public static void getKeyValue(Workbook workbook, int langValue, JSONObject jsonObject) {
+
+        for (int i = 1; i < workbook.getNumberOfSheets(); i++) {
+            Sheet sheet = workbook.getSheetAt(i);
+            for (Row row : sheet) {
+                if (!(row.getCell(0).toString().equals("") && row.getCell(langValue).toString().equals("")))
+                    CreatObjectCell(row.getCell(0), row.getCell(langValue), jsonObject);
+            }
+        }
+    }
+
+    public static void CreatObjectCell(Cell key, Cell value, JSONObject jsonObject) {
+        if (key.toString().equals("")) {
+            key.setCellValue(value.toString());
+        }
+        if (value.toString().equals((""))) {
+            value.setCellValue(key.toString());
+        }
+        jsonObject.put(key.toString(), value.toString());
+
+    }
+
+    public static void createJsonFile(String FileName, JSONObject jsonObject) {
+        try (FileWriter fileJson = new FileWriter(FileName + ".json")) {
+            fileJson.write(jsonObject.toJSONString());
+            fileJson.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String filepath = "/Users/bedir/Desktop/library/ExcelTojson/src/main/java/com/morphosium/ldd-lang.xlsx";
+
+        FileInputStream file = new FileInputStream(new File(filepath));
+        Workbook workbook = new XSSFWorkbook(file);
+
+        JSONObject test = new JSONObject();
+
+        int totalLanguage = searchLenguage(workbook).size();
+        for (int i = 1; i < searchLenguage(workbook).size(); i++) {
+            getKeyValue(workbook, i, test);
+            createJsonFile(searchLenguage(workbook).get(i).toString(), test);
+            test = new JSONObject();
+        }
+    }
+}
